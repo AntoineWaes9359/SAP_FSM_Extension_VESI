93a3f5fc0656548e055772f0fc3e6836293743b9
HZHANG <vesidev@gmail.com>
2021-09-14 21:35:37 +0000

    [FIXED] delete commented fn

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -56,8 +56,6 @@ sap.ui.define([
 					sessionStorage.setItem('token', auth.access_token);
 					setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
 				}
-				//this._initializeRefreshTokenStrategy(shellSdk, auth);
-				this.initializeRefreshTokenStrategy(shellSdk, auth)
 				var oInitFunction = {
 					property: initializeRefreshTokenStrategy
 				};
@@ -210,20 +208,6 @@ sap.ui.define([
 			}
 			// }.bind(this));
 		},
-
-		/*_initializeRefreshTokenStrategy: function(shellSdk, auth){
-			shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
-			    sessionStorage.setItem('token', event.access_token);
-			    setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
-			  });
-			  function fetchToken() {
-			    shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
-			      response_type: 'token' // request a user token within the context
-			    });
-			  }
-			  sessionStorage.setItem('token', auth.access_token);
-			  setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
-		},*/
 
 		_getEqData: function (sCloudHost, sAccount, sCompany) {
 
